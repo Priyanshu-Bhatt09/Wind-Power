aa8629432e144de9bf70740705b98e2cf2443695
Priyanshu-Bhatt09 <priyanshu.bhatt@nuvica.ai>
2026-03-15 14:45:04 +0530

    created model

---
+++ wind-backend/src/main/java/com/example/backend/model/WindDataPoint.java
@@ -0,0 +1,16 @@
+package com.example.backend.model;
+
+import lombok.Data;
+
+@Data
+public class WindDataPoint {
+    private String timestamp; //target time
+    private Integer actualGeneration;
+    private Integer forecastGeneration;
+
+    public WindDataPoint(String timestamp, Integer actualGeneration, Integer forecastGeneration) {
+        this.actualGeneration = actualGeneration;
+        this.forecastGeneration = forecastGeneration;
+        this.timestamp = timestamp;
+    }
+}
